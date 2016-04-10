3ef2a29c8ab188af045c2deb55ae283f4953a11d
ladislavlisy <ladislav.lisy@seznam.cz>
2016-04-10 09:17:49 +0200

    Initial commit

---
+++ ArticleBox/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ArticleBox
+//
+//  Created by Ladislav Lisy on 10.04.16.
+//  Copyright (c) 2016 Ladislav Lisy. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
